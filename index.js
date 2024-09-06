fb9aede8cb82c6c6faefedfaa22cda10aa824971
Devazharruk <azharruk66@gmail.com>
2024-09-05 21:51:40 -0700

    edit

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const home = require("./routes/home");
 const app = express();
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
 // Routes
 app.use("/home", home);
+
 
 // connection
 const port = process.env.PORT || 9001;
